78e803ae5bebb348e2ccbaa5768173b43c06d4e2
CleanPegasus <hazelgrace07@protonmail.com>
2024-04-14 23:05:49 +0530

    added args support

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,20 @@ use rand::{rngs::OsRng, RngCore};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::sync::atomic::{AtomicBool, Ordering};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "ethereum-address-generator", about = "Generate Ethereum addresses")]
+struct Args {
+    #[structopt(short = "s", long = "start_string", default_value = "0000")]
+    start_string: String,
+
+    #[structopt(short = "n", long = "num_threads", default_value = "4")]
+    num_threads: i32,
+
+    #[structopt(short = "e", long = "end_string", default_value = "")]
+    end_string: String,
+}
 
 fn generate_key_pair(secp: &Secp256k1<All>, rng: &mut OsRng) -> (SecretKey, PublicKey, String) {
     let mut private_key_bytes = [0_u8; 32];
@@ -26,8 +40,7 @@ fn calculate_address(public_key: &PublicKey) -> String {
     hex::encode(address_bytes)
 }
 
-
-fn worker_thread(found: Arc<AtomicBool>, attempts: Arc<Mutex<u64>>, starting_string: &str) {
+fn worker_thread(found: Arc<AtomicBool>, attempts: Arc<Mutex<u64>>, starting_string: &String, end_string: &String) {
     let secp = Secp256k1::new();
     let mut rng = OsRng;
     let mut local_attempts = 0_u64;
@@ -43,7 +56,7 @@ fn worker_thread(found: Arc<AtomicBool>, attempts: Arc<Mutex<u64>>, starting_str
             let mut attempts_lock = attempts.lock().unwrap();
             *attempts_lock += local_attempts;
         }
-        if address.starts_with(starting_string) {
+        if address.starts_with(starting_string) && address.ends_with(end_string) {
             found.store(true, Ordering::Relaxed);
             println!("Found matching Ethereum address: 0x{}", address);
             println!("Private Key: {}", private_key);
@@ -57,18 +70,22 @@ fn worker_thread(found: Arc<AtomicBool>, attempts: Arc<Mutex<u64>>, starting_str
 }
 
 fn main() {
-    let starting_string = "000000";
+    let args = Args::from_args();
+    let starting_string= args.start_string;
+    let end_string = args.end_string;
     let found = Arc::new(AtomicBool::new(false));
     let attempts = Arc::new(Mutex::new(0));
-    let num_threads = 12;
+    let num_threads = args.num_threads;
 
     let mut handles = vec![];
 
     for _ in 0..num_threads {
         let found_clone = found.clone();
         let attempts_clone = attempts.clone();
+        let starting_string_clone = starting_string.clone();
+        let ending_string_clone = end_string.clone();
         let handle = thread::spawn(move || {
-            worker_thread(found_clone, attempts_clone, starting_string);
+            worker_thread(found_clone, attempts_clone, &starting_string_clone, &ending_string_clone);
         });
         handles.push(handle);
     }
